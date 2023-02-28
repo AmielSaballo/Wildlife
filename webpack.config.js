5ccab4ef2e5570952f3076f2df38633ac977c486
AmielSaballo <saballoamiel@gmail.com>
2023-02-28 10:44:22 +0800

    modified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,59 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
+module.exports = (env) => {
+  const API_URL = env.development ? "" : "";
+  const vars = {
+    "process.env.API_URL": JSON.stringify(API_URL),
+  };
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "build"),
     },
-    port: 3000,
-    historyApiFallback: true,
-  },
-  module: {
-    // exclude node_modules
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|webp|jfif)$/,
-        type: "asset/resource",
-      },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
     ],
-  },
-  // pass all js files through Babel
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "build"),
+      },
+      port: 3000,
+      historyApiFallback: true,
+    },
+    module: {
+      // exclude node_modules
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+            { loader: "sass-loader" },
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webp|jfif)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    // pass all js files through Babel
+    resolve: {
+      extensions: ["*", ".js", ".jsx"],
+    },
+  };
 };
