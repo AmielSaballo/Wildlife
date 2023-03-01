c5ccb536a0e6ed610e5398288bc5ae3e762b80c4
AmielSaballo <saballoamiel@gmail.com>
2023-03-01 20:51:55 +0800

    Form post now working

--- src/Assets/services.js
+++ src/Assets/services.js
@@ -2,3 +2,5 @@ const baseURL = "https://amiel-wildlifeapi.azurewebsites.net/api/";
 export const ProgramsAPI = baseURL + "Programs";
 export const AnimalsAPI = baseURL + "Animals";
 export const BlogsAPI = baseURL + "Blogs";
+export const VolunteerAPI = baseURL + "Volunteers";
+export const ContactAPI = baseURL + "Contacts";
